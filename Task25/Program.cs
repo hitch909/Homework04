7b161306cae6495d7d76c50e0bded0d3a121b61b
hitch909 <evgenij909@yandex.ru>
2023-01-28 14:49:18 +0500

    25 задача готова.

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿// функцию, которая принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+Console.Clear();
+Console.Write("введите число A= ");
+int A = int.Parse(Console.ReadLine()!);
+Console.Write("введите второе число B= ");
+int B = int.Parse(Console.ReadLine()!);
+
+void Exponentiation (int A, int B)
+{
+int result = 1;
+for (int i = 1; i <= B; i++) result = result * A;
+Console.WriteLine($"введенное число {A} возведенное в степень {B} = {result}");
+}
+Exponentiation(A,B);
