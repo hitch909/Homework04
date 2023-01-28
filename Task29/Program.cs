9640eb2f4d48423ccbcfe97707fd2db471e7f020
hitch909 <evgenij909@yandex.ru>
2023-01-28 15:18:27 +0500

    29 задача готова.

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// программа, которая задаёт массив из 8 элементов 
+// и выводит их на экран.
+
+int n = 8;
+int[] array = new int[n];
+void PrintArray(int[] array)
+{
+    Console.Write($" {array[1]}");  // избавился от запятой
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = new Random().Next(0, n);
+        Console.Write($",{array[i]}");
+    }
+}
+PrintArray(array);
